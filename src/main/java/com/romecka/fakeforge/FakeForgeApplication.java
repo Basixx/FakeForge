8c0dd0c52cc5e255f0f35ac969bf0190970332d0
barbararomecka <basienkarutkowska@gmail.com>
2025-05-29 15:50:03 +0200

    Initial commit

---
+++ src/main/java/com/romecka/fakeforge/FakeForgeApplication.java
@@ -0,0 +1,13 @@
+package com.romecka.fakeforge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FakeForgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FakeForgeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/romecka/fakeforge/FakeForgeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.romecka.fakeforge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FakeForgeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
