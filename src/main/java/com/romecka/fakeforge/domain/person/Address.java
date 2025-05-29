310d5dce899902b16160b826a9f5bf5da740c3ca
barbararomecka <basienkarutkowska@gmail.com>
2025-05-29 20:37:38 +0200

    Add addresses table

---
+++ src/main/java/com/romecka/fakeforge/domain/person/Address.java
@@ -0,0 +1,38 @@
+package com.romecka.fakeforge.domain.person;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotNull;
+
+import static jakarta.persistence.GenerationType.IDENTITY;
+
+@Entity(name = "addresses")
+public record Address(
+
+        @Id
+        @GeneratedValue(strategy = IDENTITY)
+        @Column(unique = true)
+        @NotNull
+        Long id,
+
+        @Column(length = 50)
+        @NotNull
+        String street,
+
+        @NotNull
+        int buildingNumber,
+
+        @NotNull
+        int apartmentNumber,
+
+        @Column(length = 6)
+        @NotNull
+        String postalCode,
+
+        @Column(length = 20)
+        @NotNull
+        String city
+) {
+}
