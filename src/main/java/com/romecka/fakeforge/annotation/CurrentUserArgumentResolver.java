cb340aad2694537c44915c736c61d26d0e8feae3
barbararomecka <basienkarutkowska@gmail.com>
2025-06-05 20:30:20 +0200

    Add CurrentUserArgument to endpoints

---
+++ src/main/java/com/romecka/fakeforge/annotation/CurrentUserArgumentResolver.java
@@ -0,0 +1,31 @@
+package com.romecka.fakeforge.annotation;
+
+import com.romecka.fakeforge.domain.entities.User;
+import jakarta.servlet.http.HttpServletRequest;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.core.MethodParameter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+@Component
+public class CurrentUserArgumentResolver implements HandlerMethodArgumentResolver {
+
+    @Override
+    public boolean supportsParameter(MethodParameter parameter) {
+        return parameter.getParameterAnnotation(CurrentUser.class) != null
+                && parameter.getParameterType().equals(User.class);
+    }
+
+    @Override
+    public Object resolveArgument(@NotNull MethodParameter parameter,
+                                  ModelAndViewContainer mavContainer,
+                                  NativeWebRequest webRequest,
+                                  WebDataBinderFactory binderFactory) {
+        HttpServletRequest request = (HttpServletRequest) webRequest.getNativeRequest();
+        return request.getAttribute("currentUser");
+    }
+
+}
--- src/main/java/com/romecka/fakeforge/controller/LimitController.java
+++ src/main/java/com/romecka/fakeforge/controller/LimitController.java
@@ -1,14 +1,13 @@
 package com.romecka.fakeforge.controller;
 
+import com.romecka.fakeforge.annotation.CurrentUser;
 import com.romecka.fakeforge.domain.dto.LimitDto;
+import com.romecka.fakeforge.domain.entities.User;
 import com.romecka.fakeforge.facade.LimitFacade;
-import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Optional;
 
 import static org.springframework.http.HttpStatus.OK;
 
@@ -20,12 +19,8 @@ public class LimitController {
 
     @GetMapping(value = "/users/limit")
     @ResponseStatus(OK)
-    public LimitDto getUserLimit(HttpServletRequest request) {
-        Long userId = Optional.ofNullable(request.getAttribute("userId"))
-                .map(Object::toString)
-                .map(Long::parseLong)
-                .orElse(null);
-        return limitFacade.getUserLimit(userId);
+    public LimitDto getUserLimit(@CurrentUser User user) {
+        return limitFacade.getUserLimit(user.getId());
     }
 
 }
