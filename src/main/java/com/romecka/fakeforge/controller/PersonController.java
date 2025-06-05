aa978e46601607d639c88246348a7e54451026bd
barbararomecka <basienkarutkowska@gmail.com>
2025-06-05 21:06:48 +0200

    Add CurrentUserArgument to persons endpoint

--- src/main/java/com/romecka/fakeforge/controller/PersonController.java
+++ src/main/java/com/romecka/fakeforge/controller/PersonController.java
@@ -1,8 +1,9 @@
 package com.romecka.fakeforge.controller;
 
+import com.romecka.fakeforge.annotation.CurrentUser;
 import com.romecka.fakeforge.domain.dto.PersonDto;
+import com.romecka.fakeforge.domain.entities.User;
 import com.romecka.fakeforge.facade.PersonFacade;
-import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -10,7 +11,6 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-import java.util.Optional;
 
 import static org.springframework.http.HttpStatus.OK;
 
@@ -22,14 +22,10 @@ public class PersonController {
 
     @GetMapping(value = "/users/persons")
     @ResponseStatus(OK)
-    public List<PersonDto> getPersonsByUser(HttpServletRequest request,
+    public List<PersonDto> getPersonsByUser(@CurrentUser User user,
                                             @RequestParam(defaultValue = "0") int page,
                                             @RequestParam(defaultValue = "10") int size) {
-        Long userId = Optional.ofNullable(request.getAttribute("userId"))
-                .map(Object::toString)
-                .map(Long::parseLong)
-                .orElse(null);
-        return personFacade.getPersonsByUserId(userId, page, size);
+        return personFacade.getPersonsByUserId(user.getId(), page, size);
     }
 
 }
