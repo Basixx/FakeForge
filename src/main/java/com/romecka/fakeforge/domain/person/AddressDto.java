1ce0a000d7f23aa5eed3b3062c07743b27288f4f
barbararomecka <basienkarutkowska@gmail.com>
2025-06-24 21:05:30 +0200

    Remove address Dto and mapper

--- src/main/java/com/romecka/fakeforge/domain/person/AddressDto.java
+++
@@ -1,9 +0,0 @@
-package com.romecka.fakeforge.domain.person;
-
-public record AddressDto(String street,
-                         int buildingNumber,
-                         int apartmentNumber,
-                         String postalCode,
-                         String city) {
-
-}
