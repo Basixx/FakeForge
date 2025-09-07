9f8f4abd3d17b575d2d365f6ea7dec2dcb9ee43b
barbararomecka <basienkarutkowska@gmail.com>
2025-09-07 10:50:48 +0200

    Add limit reset

--- src/main/java/com/romecka/fakeforge/infrastructure/db/limit/LimitRepository.java
+++ src/main/java/com/romecka/fakeforge/infrastructure/db/limit/LimitRepository.java
@@ -1,6 +1,8 @@
 package com.romecka.fakeforge.infrastructure.db.limit;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -9,5 +11,9 @@ import java.util.Optional;
 public interface LimitRepository extends JpaRepository<LimitEntity, Long> {
 
     Optional<LimitEntity> findByUserId(long userId);
+
+    @Modifying
+    @Query("UPDATE LimitEntity l SET l.availableLimit = l.dailyLimit")
+    void resetAllLimits();
 
 }
