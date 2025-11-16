184d8e34b5fcd99547544d56c414d0d214df6a61
barbararomecka <basienkarutkowska@gmail.com>
2025-11-16 09:57:01 +0100

    Change order of persons list

--- src/main/java/com/romecka/fakeforge/infrastructure/db/person/PersonRepository.java
+++ src/main/java/com/romecka/fakeforge/infrastructure/db/person/PersonRepository.java
@@ -9,11 +9,11 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface PersonRepository extends JpaRepository<PersonEntity, Long> {
 
-    Slice<PersonEntity> findByUserIdOrderByIdAsc(long userId, Pageable pageable);
+    Slice<PersonEntity> findByUserIdOrderByIdDesc(long userId, Pageable pageable);
 
     default Slice<PersonEntity> findByUserId(long userId, int page, int size) {
         Pageable pageable = PageRequest.of(page, size);
-        return findByUserIdOrderByIdAsc(userId, pageable);
+        return findByUserIdOrderByIdDesc(userId, pageable);
     }
 
 }
